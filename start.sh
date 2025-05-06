a7ec9edeb214404c5809d0e28dd22b85d94a3143
swilliamsilva <simuleagora@gmail.com>
2025-05-06 10:35:47 -0300

    Atualização

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Aguardando o PostgreSQL iniciar..."
 sleep 10
-java -jar /cpmatmed/backend.jar  # Caminho absoluto para garantir
+java -jar /app/backend.jar  # Caminho absoluto para garantir
