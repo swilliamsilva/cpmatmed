448dd19711e2e82a0e424d79af7ebf6ff1d2060f
swilliamsilva <simuleagora@gmail.com>
2025-05-08 08:51:22 -0300

    Atualização.

--- backend/src/main/java/com/cpmatmed/backend/CpmatmedBackendApplication.java
+++ backend/src/main/java/com/cpmatmed/backend/CpmatmedBackendApplication.java
@@ -7,7 +7,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.cpmatmed.backend.repository")
 public class CpmatmedBackendApplication {
+
     public static void main(String[] args) {
+          //  Teste de variáveis de ambiente antes de subir o Spring
+          System.out.println("SPRING_DATASOURCE_URL: " + System.getenv("SPRING_DATASOURCE_URL"));
+          System.out.println("SPRING_DATASOURCE_USERNAME: " + System.getenv("SPRING_DATASOURCE_USERNAME"));
+          System.out.println("SPRING_DATASOURCE_PASSWORD: " + System.getenv("SPRING_DATASOURCE_PASSWORD"));
+
+
         SpringApplication.run(CpmatmedBackendApplication.class, args);
     }
 }
